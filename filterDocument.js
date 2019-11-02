b638b078cfba077137b57df820bd85de9ab1481a
femtioelva <naivigera@gmail.com>
2019-11-02 21:01:17 +0100

    type, endpoints

--- filterDocument.js
+++ filterDocument.js
@@ -1,14 +1,15 @@
-const filters = ["brinner", "brand", "skogsbrand", "eld", "eldssvåda"];
+const filters = [["brinner", "fire"], ["brand", "fire"], ["skogsbrand", "fire"], ["eld", "fire"], ["eldssvåda", "fire"], ["olycka", "accident"]];
 const natural = require("natural");
 const tokenizer = new natural.AggressiveTokenizerSv();
 
-module.exports = function (documentDescription) {
+module.exports = function filterDocument(documentDescription) {
         const documentLowerCase = documentDescription.toLowerCase();
-        const tokens = tokenizer.tokenize(documentDescription);
-        for(let filter of filters) {
-            if (documentLowerCase.includes(filter)) {
-                return true;
+        const tokens = tokenizer.tokenize(documentLowerCase);
+        const filterMap = new Map(filters);
+        for(let filter of filterMap.keys()) {
+            if (tokens.includes(filter)) {
+                return filterMap.get(filter);
             }
         }
-        return false;
+        return null;
     }
