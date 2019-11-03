bd5b18cea8041c698afa3092525b3ebb91add148
femtioelva <naivigera@gmail.com>
2019-11-03 10:48:49 +0100

    format, add timeout for fetching data

--- filterDocument.js
+++ filterDocument.js
@@ -1,7 +1,11 @@
-const filters = [["brinner", "fire"], ["brand", "fire"], ["skogsbrand", "fire"], ["eld", "fire"], ["eldssvåda", "fire"], ["olycka", "accident"], ["olyckor", "accident"], ["trafikolycka", "accident"], ["krock", "accident"]];
 const natural = require("natural");
 const tokenizer = new natural.AggressiveTokenizerSv();
+const filters = [["brinner", "fire"], ["brand", "fire"], ["skogsbrand", "fire"], ["eld", "fire"], ["eldssvåda", "fire"], ["olycka", "accident"], ["olyckor", "accident"], ["trafikolycka", "accident"], ["krock", "accident"]];
 
+/**
+ * documentDescription is the content/string/data to evaluate
+ * will return type, if found, based on filters.
+ */
 module.exports = function filterDocument(documentDescription) {
         const documentLowerCase = documentDescription.toLowerCase();
         const tokens = tokenizer.tokenize(documentLowerCase);
